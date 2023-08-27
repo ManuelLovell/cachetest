e442d540603df12d7e8161bba0bc86b25d14efd7
Manuel DeJesus <69539934+ManuelLovell@users.noreply.github.com>
2023-08-27 11:58:46 -0700

    Cache Testing

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import path from 'path'
+
+export default {
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      }
+    }
+  }
+}
