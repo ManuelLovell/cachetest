11def73df89d6429e983283e68f4dcc8f82dda65
ManuelLovell <69539934+ManuelLovell@users.noreply.github.com>
2023-08-27 19:49:14 +0000

    Build: (84ae164) test5

--- assets/main-0d122808.js
+++ assets/main-0d122808.js
@@ -11,7 +11,7 @@
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-    First test. Second test. Third Test
+    First test. Second test. Third Test.FourthTest
     </p>
   </div>
 `;u(document.querySelector("#counter"));
